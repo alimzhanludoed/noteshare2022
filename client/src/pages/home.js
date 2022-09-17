d14c62547717bed738e09b42b13947a40ca92562
agarwalaswastik <agarwalaswastik@gmail.com>
2022-09-17 19:11:28 -0400

    DID NOTHING

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -13,7 +13,14 @@ export default function Home() {
             <br />
             <div className="container">
                 <div className='back'>
-                    <CourseCard name='Multivariate Calculus' logo={Logo} instructor='Andrey Glubukov' university='Purdue University'/>
+                    <CourseCard name='Multivariate Calculus' logo={Logo} instructor='Tyler Potler' university='Purdue University'/>
+                    <CourseCard name='Elementry Psychology' logo={Logo} instructor='Marco Zhang' university='Purdue University'/>
+                    <CourseCard name='Microeconomics' logo={Logo} instructor='Dev' university='Purdue University'/>
+                    <CourseCard name='CS Orientation' logo={Logo} instructor='Swastik Agarwala' university='Purdue University'/>
+                    <div className="line"></div>
+                    <CourseCard name='Macroeconomics' logo={Logo} instructor='Anirudh Kaza' university='Purdue University'/>
+                    <CourseCard name='General Chemistry' logo={Logo} instructor='Andrew' university='Purdue University'/>
+                    <CourseCard name='Biotechnology' logo={Logo} instructor='Jennifer Liu' university='Purdue University'/>
                 </div>
             </div>
         </>
