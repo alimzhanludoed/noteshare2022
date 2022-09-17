cfc1eb0e11047ec5bf53236ea07faf8b56a2729f
agarwalaswastik <agarwalaswastik@gmail.com>
2022-09-17 16:02:19 -0400

    Home Page Progress

---
+++ client/src/components/coursecard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './coursecard.css';
+
+export default function CourseCard(props) {
+    
+    return (
+        <div className='cardbackground'>
+            <img className='courseimage' src={props.logo} alt={props.name} />
+            <div className='container'>
+                <div className="cardcontent">
+                    {props.name}
+                    <br />
+                    <br />
+                    <br />
+                    <span className='content'>
+                        {props.instructor}
+                        <br />
+                        {props.university}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import './home.css';
+import CourseCard from '../components/coursecard';
+import Logo from '../images/logo.png';
 
 export default function Home() {
     return (
@@ -11,7 +13,7 @@ export default function Home() {
             <br />
             <div className="container">
                 <div className='back'>
-                    
+                    <CourseCard name='Multivariate Calculus' logo={Logo} instructor='Andrey Glubukov' university='Purdue University'/>
                 </div>
             </div>
         </>
