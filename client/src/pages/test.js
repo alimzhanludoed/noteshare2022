d4531870351672c2fe91f5acff5e84ec84c81f2b
agarwalaswastik <agarwalaswastik@gmail.com>
2022-09-18 00:36:47 -0400

    Updated Test

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -2,14 +2,27 @@ import React from 'react';
 import './test.css'
 import { useParams } from 'react-router-dom';
 import Navbar from '../components/navbar';
+import axios from 'axios';
 
 export default function Test() {
 
   const {coursename, section} = useParams();
 
   function uploadPDF() {
-    const link = prompt('Enter link (Make sure that the document is public)');
-
+    const doclink = prompt('Enter link (Make sure that the document is public)');
+    if (doclink == null) {}
+    else {
+      axios.post(
+        'http://localhost:8000/docs', {
+          course: coursename+"/"+section,
+          link: doclink
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        },
+        )
+    }
   }
 
   return (
@@ -19,7 +32,6 @@ export default function Test() {
         <h4>{coursename} {section}</h4>
         <button className='uploadbutton' onClick={uploadPDF}>Upload</button>
       </div>
-      
       <hr />
     </div>
   );
