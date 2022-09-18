16c0cf0312a7dd1f3ea2374b31620305dae3f503
agarwalaswastik <agarwalaswastik@gmail.com>
2022-09-18 00:20:08 -0400

    Added Popup

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -7,13 +7,19 @@ export default function Test() {
 
   const {coursename, section} = useParams();
 
+  function uploadPDF() {
+    const link = prompt('Enter link (Make sure that the document is public)');
+
+  }
+
   return (
     <div className="test">
       <Navbar />
       <div className='maindiv'>
         <h4>{coursename} {section}</h4>
-        <button className='uploadbutton'>Upload</button>
+        <button className='uploadbutton' onClick={uploadPDF}>Upload</button>
       </div>
+      
       <hr />
     </div>
   );
